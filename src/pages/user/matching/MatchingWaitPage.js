c4561f81c1d2b51da469d08ff2c487187f2ac94d
hyuk <k941026h@naver.com>
2024-02-13 22:21:07 +0900

    PUSH 알림 받기 버튼 위치 변경

--- src/pages/user/matching/MatchingWaitPage.js
+++ src/pages/user/matching/MatchingWaitPage.js
@@ -1,13 +1,15 @@
 import { Button } from "@mui/material";
 import PromptText from "components/common/PromptText";
 import { requestPermission } from "config/fcmConfig";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { scrollAble } from "util";
 import { scrollDisable } from "util";
 
 function MatchingWaitPage(props) {
 	const title = "잠시만 기다려주세요!";
 	const subtitle = `상대방이 요청을 확인중입니다.`;
+
+	const [pushSetting, setPushSetting] = useState(false);
 
 	useEffect(() => {
 		scrollDisable();
@@ -19,16 +21,27 @@ function MatchingWaitPage(props) {
 
 	return (
 		<>
-			<Button
-				onClick={() => {
-					/**
-					 * 1. Firebase register
-					 */
-					requestPermission();
-				}}>
-				푸시알람 허용
-			</Button>
 			<PromptText title={title} subtitle={subtitle} />
+			<Button
+				sx={{
+					position: "absolute",
+					left: "0",
+					padding: 0,
+					bottom: "15vh",
+					width: "100vw",
+				}}
+				onClick={async () => {
+					if (!pushSetting) {
+						/**
+						 * 1. Firebase register
+						 */
+						setPushSetting(true);
+						await requestPermission();
+						setPushSetting(false);
+					}
+				}}>
+				PUSH 알림 받기
+			</Button>
 		</>
 	);
 }
